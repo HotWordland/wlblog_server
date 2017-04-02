79cefb8290929520ad59d6fb6fc10203eb7d5b61
HotWordland <454763196@qq.com>
2017-04-02 19:59:31 +0800

    # 移除一些文件

--- Public/admin/src/View/IntroView/IntroView.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Ronaldinho on 15/8/27.
- */
-'use strict';
-
-angular.module('MyApp.IntroView',[]).controller('IntroViewCtl',function IntroViewCtl(){
-   var vm = this;
-   vm.introView_name = "hello";
-});
